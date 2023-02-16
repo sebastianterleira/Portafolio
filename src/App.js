10f5e1e9c835613d2112efb4264c99f263876174
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-16 12:19:49 -0500

    Using Theme Provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { ThemeProvider } from "styled-components"
+import { LightTheme, DarkTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+
 function App() {
-  return <div>
-    App File
-    </div>
+  return (
+    <>
     
+    <GlobalStyle />
+
+      <ThemeProvider theme={LightTheme}>|
+        App File
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App
