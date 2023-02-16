6f06c5e9abd825189f5994d98783b6843e2d47f9
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-16 14:15:15 -0500

    Enrutamiento de los archivos requeridos para cada sección

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { ThemeProvider } from "styled-components"
-import { LightTheme, DarkTheme } from "./components/Themes"
-import GlobalStyle from "./globalStyles"
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { LightTheme, DarkTheme } from "./components/Themes";
+import GlobalStyle from "./globalStyles";
+
+// Componentes
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import Main from "./components/Main";
+import MySkillsPage from "./components/MySkillsPage";
+import WorkPage from "./components/WorkPage";
 
 function App() {
   return (
     <>
-    
     <GlobalStyle />
-
-      <ThemeProvider theme={LightTheme}>|
-        App File
+      <ThemeProvider theme={LightTheme}>
+        <Routes>
+          <Route exact path={"/"} element={<Main/>}/>
+          <Route path={"AboutPage"} element={<AboutPage/>}/>
+          <Route path={"BlogPage"} element={<BlogPage/>}/>
+          <Route path={"WorkPage"} element={<WorkPage/>}/>
+          <Route path={"MySkillsPage"} element={<MySkillsPage/>}/>
+        </Routes>
       </ThemeProvider>
     </>
   )
