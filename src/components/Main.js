cfe82aba33226724bbd854b9e2f2fe3aa57c0b62
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-16 16:46:42 -0500

    subComponent => LogoComponent (finish)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import LogoComponent from "../subComponents/LogoComponent";
+import PowerButton from "../subComponents/PowerButton";
+
+const MainContainer = styled.div`
+background: ${props => props.theme.body};
+width: 100vw;
+height: 100vh;
+overflow: hidden;
+
+position: relative;
+
+h2,h3,h4,h5,h6{
+  font-family:'Karla', sans-serif;
+  font-weight: 500;
+}
+`
+
+const Container = styled.div`
+padding: 2rem;
+`
 
 function Main() {
   return (
-    <>
-    Main Component/Page
-    </>
+    <MainContainer>
+      <Container>
+        <PowerButton />
+        <LogoComponent />
+      </Container>
+    </MainContainer>
   )
 }
 
