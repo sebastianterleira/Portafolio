e90ca6f3bc691cc909060798f1271b5f5df5f631
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-16 21:36:35 -0500

    NavLinks hacia mis diferentes secciones

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
@@ -22,6 +23,58 @@ const Container = styled.div`
 padding: 2rem;
 `
 
+const Contact = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 2rem;
+right: calc(1rem + 2vw);
+text-decoration: none;
+z-index: 1;
+`
+
+const BLOG = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 50%;
+right: calc(1rem + 2vw);
+transform: rotate(90deg) translate(-50%, -50%);
+text-decoration: none;
+z-index: 1;
+`
+
+const WORK = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 50%;
+left: calc(1.147rem + 2vw);
+transform: translate(-50%, -50%) rotate(-90deg);
+text-decoration: none;
+z-index: 1;
+`
+
+const BottomBar = styled.div`
+position: absolute;
+bottom: 1rem;
+left: 0;
+right: 0;
+width: 100%;
+
+display: flex;
+justify-content: space-evenly;
+`
+
+const ABOUT = styled(NavLink)`
+color: ${props => props.theme.text};
+text-decoration: none;
+z-index: 1;
+`
+
+const SKILLS = styled(NavLink)`
+color: ${props => props.theme.text};
+text-decoration: none;
+z-index: 1;
+`
+
 function Main() {
   return (
     <MainContainer>
@@ -29,6 +82,35 @@ function Main() {
         <PowerButton />
         <LogoComponent />
         <SocialIcons />
+
+        <Contact target="_blank" to={{pathname:"mailto:seb.terleira1204@gmail.com"}}>
+          <h3>
+            Say hi..
+          </h3>
+        </Contact>
+        <BLOG to={"/blog"}>
+          <h2>
+            Blog
+          </h2>
+        </BLOG>
+        <WORK to={"/work"}>
+          <h2>
+            Work
+          </h2>
+        </WORK>
+        <BottomBar>
+        <ABOUT to={"/about"}>
+          <h2>
+            About.
+          </h2>
+        </ABOUT>
+        <SKILLS to={"/skills"}>
+          <h2>
+            My Skills.
+          </h2>
+        </SKILLS>
+        </BottomBar>
+
       </Container>
     </MainContainer>
   )
