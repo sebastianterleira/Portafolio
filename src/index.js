690d715a9a95bc7cef5d7afbfd274a633e8ab8fa
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-16 21:42:10 -0500

    Moving file Normalize.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 // import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-import './normalize.css'
+import './dependences/normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
