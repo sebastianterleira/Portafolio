62a6635d5f7c8d5431018df43b79a46628bd4987
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-18 12:58:35 -0500

    New BlogData

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -4,48 +4,48 @@ export const Blogs = [
     {
         id:1,
         name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
-        tags:["react","gsap","styled-components"],
-        date:"13 May, 2023",
+        tags:["Blog", "History", "Article"],
+        date:"25 Dec, 2022",
         imgSrc:"Mi Historia",
         link:"Insertar Link"
     },
     {
         id:2,
         name:"Articulo Por Rellenar",
-        tags:["react","styled-components"],
-        date:"5 July, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
     },
     {
         id:3,
         name:"Articulo Por Rellenar",
-        tags:["react","react-lottie","styled-components"],
-        date:"6 July, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
     },
     {
         id:4,
         name:"Articulo Por Rellenar",
-        tags:["react","redux","framer-motion"],
-        date:"26 May, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
     },
     {
         id:5,
         name:"Articulo Por Rellenar",
-        tags:["react","reactJS","web-development"],
-        date:"3 Feb, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
     },
     {
         id:6,
         name:"Articulo Por Rellenar",
-        tags:["react","css"],
-        date:"5 Jan, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
 
@@ -54,16 +54,16 @@ export const Blogs = [
     {
         id:7,
         name:"Articulo Por Rellenar",
-        tags:["react","redux","javascript"],
-        date:"16 May, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
     },
     {
         id:8,
         name:"Articulo Por Rellenar",
-        tags:["react","framer-motion","styled-components"],
-        date:"26 May, 2023",
+        tags:[],
+        date:"No Date",
         imgSrc:"Insertar Imagen",
         link:"Insertar Link"
     },
