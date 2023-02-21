8dd625183b20fe4c262459fae248f0a1546ab83d
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-20 20:39:00 -0500

    LazyLoading and Suspense

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
+import React, {Suspense, lazy} from "react";
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { LightTheme, DarkTheme } from "./components/Themes";
 import GlobalStyle from "./globalStyles";
+import LazyLoading from "./components/LazyLoading";
+import { AnimatePresence } from "framer-motion";
 
 // Componentes
-import AboutPage from "./components/AboutPage";
-import BlogPage from "./components/BlogPage";
-import Main from "./components/Main";
-import MySkillsPage from "./components/MySkillsPage";
-import WorkPage from "./components/WorkPage";
-import { AnimatePresence } from "framer-motion";
 import SoundBar from "./subComponents/SoundBar";
+const Main = lazy(() => import("./components/Main"));
+const AboutPage = lazy(() => import("./components/AboutPage"));
+const MySkillsPage = lazy(() => import("./components/MySkillsPage"));
+const BlogPage = lazy(() => import("./components/BlogPage"));
+const WorkPage = lazy(() => import("./components/WorkPage"));
+
 
 function App() {
-
-  return (
-    <>
-    <GlobalStyle />
-      <ThemeProvider theme={LightTheme}>
-        <SoundBar />
-        {/* For Framer-motion animation on page change */}
-        <AnimatePresence exitBeforeEnter>
-          <Routes >
-            <Route exact path={"/"} element={<Main/>}/>
-            <Route path={"about"} element={<AboutPage/>}/>
-            <Route path={"blog"} element={<BlogPage/>}/>
-            <Route path={"work"} element={<WorkPage/>}/>
-            <Route path={"skills"} element={<MySkillsPage/>}/>
-          </Routes>
-        </AnimatePresence>
-      </ThemeProvider>
-    </>
-  )
+    return (
+      <>
+      <GlobalStyle />
+        <ThemeProvider theme={LightTheme}>
+          <SoundBar />
+          {/* For Framer-motion animation on page change */}
+          <AnimatePresence exitBeforeEnter>
+            <Routes >
+              <Route exact path={"/"} element={
+              <Suspense fallback={<LazyLoading />}>
+                <Main/>
+              </Suspense>
+              }/>
+              <Route path={"about"} element={
+              <Suspense fallback={<LazyLoading />}>
+                <AboutPage/>
+              </Suspense>
+              }/>
+              <Route path={"skills"} element={
+              <Suspense fallback={<LazyLoading />}>
+                <MySkillsPage/>
+              </Suspense>
+              }/>
+              <Route path={"blog"} element={<BlogPage/>}/>
+              <Route path={"work"} element={<WorkPage/>}/>
+            </Routes>
+          </AnimatePresence>
+        </ThemeProvider>
+      </>
+    )
 }
 
 export default App
