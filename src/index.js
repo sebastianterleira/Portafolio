0cf1d7ca28f75d5c08ecf27c9040471f48e6abed
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-23 11:56:48 -0500

    (ADD) Normalize.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import "./dependences/normalize.css"
+import "./reset/normalize.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
